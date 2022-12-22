dd47da45ae2058afa5cd222b8994bbbf4432c3c4
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2022-12-22 22:16:38 +0500

    all function are done.

--- recipes/urls.py
+++ recipes/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 
 from django.urls import path
 
-from calculator.views import get_omlet
+from calculator.views import get_omlet, get_pasta, get_buter
 
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
     path('omlet/', get_omlet),
-]
+    path('pasta/', get_pasta),
+    path('buter/', get_buter),
+    ]
+
